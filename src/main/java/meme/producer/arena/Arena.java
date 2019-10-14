6b00aa2bef5d5701bbd0a514a2dd6c1817a45d76
hpuyang7942 <hpuyang@connect.ust.hk>
2019-10-14 16:39:21 +0800

    PUYANG Huancheng lab5 test

--- src/main/java/meme/producer/arena/Arena.java
+++ src/main/java/meme/producer/arena/Arena.java
@@ -1,4 +1,9 @@
 package meme.producer.arena;
+import meme.producer.towers.Tower;
+import meme.producer.monsters.Monster;
 
 public class Arena {
+    public static void hpuyang(String py) {
+        System.out.println("haha");
+    }
 }
